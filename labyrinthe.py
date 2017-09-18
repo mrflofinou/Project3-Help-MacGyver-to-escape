43b5871ab1c72d07a47434f03914b26d5b296a52
Florian <florian.lescuyer@gmail.com>
2017-09-18 15:17:46 +0200

    update of labyrinthe.py file to display the game board without pygame

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,7 +1,21 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+"""
+Project 3 of Python web developer course of OpenClassrooms
+This is a game where we have to help MacGyver to escape
+Mac Gyver must find 3 items to fight the guardian of the labyrinth
+"""
+
+import classes
+
 def main():
+    """
+    Main function of the programm
+    """
+    board = classes.Board()
+    board.initialization()
+    board.display()
 
 
 if __name__ == "__main__":
