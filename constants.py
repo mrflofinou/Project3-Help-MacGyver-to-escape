09353d2337f86a8044af25618607ce3966adc18a
Florian <florian.lescuyer@gmail.com>
2017-09-21 15:52:16 +0200

    add paths of pictures and constants for pygame

---
+++ constants.py
@@ -0,0 +1,15 @@
+"""
+This file contains constants of game and paths
+of pictures
+"""
+
+number_cases_side = 15
+size_case = 40
+
+title = "MacGyver - The Game"
+floor = "pictures/floor.png"
+walls = "pictures/walls.png"
+item = "pictures/item.png"
+macgyver = "pictures/macgyver.png"
+murdock = "pictures/murdock.png"
+stairs = "pictures/stairs.png"
