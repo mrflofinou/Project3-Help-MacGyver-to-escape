e617541d905d8d135b2dd0d84e2cd5ce5757d033
Florian <florian.lescuyer@gmail.com>
2017-09-21 17:12:43 +0200

    modifications in the loop to display characters

--- labyrinth.py
+++ labyrinth.py
@@ -20,24 +20,31 @@ def main():
     #initialization of pygame module
     pygame.init()
     #creation of window for display
-    size_side = constants.number_cases_side*constants.size_case
+    size_side = constants.number_cases_side * constants.size_sprite
     window = pygame.display.set_mode((size_side, size_side))
     #Title of window
     pygame.display.set_caption(constants.title)
+    #creation of the object board from class Board
     board = game.Board()
-    mac = game.Characters()
+    #creation of object mac from class Macgyver
+    mac = game.Macgyver()
+    #creation of object murdock from class Murdock
+    murdock = game.Murdock()
     #Display the labyrinth in the window
     board.display(window)
     play = 1
     while play:
+        #to limit the framerate
+        pygame.time.Clock().tick(30)
         # direction = input("choose the direction: ")
         # mac.move(direction, board)
         # board.display(mac)
         # if board.STRUCTURE[mac.line][mac.column] == "a":
         #     print("\nYOU WIN !")
         #     break
+        window.blit(mac.avatar, (mac.pixels_x, mac.pixels_y))
+        window.blit(murdock.avatar, (murdock.pixels_x, murdock.pixels_y))
         pygame.display.flip()
-        continue
 
 if __name__ == "__main__":
     main()
