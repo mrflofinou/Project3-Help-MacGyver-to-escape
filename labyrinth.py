cbc29c55f31fd34a734a3a5590a47142b4898b08
Florian <florian.lescuyer@gmail.com>
2017-09-21 18:08:32 +0200

    modification of the loop to manage the movement with pygame

--- labyrinth.py
+++ labyrinth.py
@@ -24,27 +24,39 @@ def main():
     window = pygame.display.set_mode((size_side, size_side))
     #Title of window
     pygame.display.set_caption(constants.title)
+    #this is for the character stay in movement whent you stay press on a key
+    pygame.key.set_repeat(400, 30)
     #creation of the object board from class Board
     board = game.Board()
     #creation of object mac from class Macgyver
     mac = game.Macgyver()
     #creation of object murdock from class Murdock
     murdock = game.Murdock()
-    #Display the labyrinth in the window
-    board.display(window)
     play = 1
     while play:
         #to limit the framerate
         pygame.time.Clock().tick(30)
-        # direction = input("choose the direction: ")
-        # mac.move(direction, board)
-        # board.display(mac)
-        # if board.STRUCTURE[mac.line][mac.column] == "a":
-        #     print("\nYOU WIN !")
-        #     break
+        #management of keys with pygame
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                play = 0
+            if event.type == KEYDOWN:
+                if event.key == K_RIGHT:
+                    mac.move('right', board)
+                if event.key == K_LEFT:
+                    mac.move('left', board)
+                if event.key == K_UP:
+                    mac.move('up', board)
+                if event.key == K_DOWN:
+                    mac.move('down', board)
+        #Display the labyrinth in the window
+        board.display(window)
         window.blit(mac.avatar, (mac.pixels_x, mac.pixels_y))
         window.blit(murdock.avatar, (murdock.pixels_x, murdock.pixels_y))
         pygame.display.flip()
+        if board.STRUCTURE[mac.line][mac.column] == "a":
+            print("\nYOU WIN !")
+            play = 0
 
 if __name__ == "__main__":
     main()
