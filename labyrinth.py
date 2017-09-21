fa5226a9ea3f3eec6948fe4b7c567408b3c1b2a0
Florian <florian.lescuyer@gmail.com>
2017-09-21 15:54:19 +0200

    modification for initialization of window for pygame

--- labyrinth.py
+++ labyrinth.py
@@ -7,23 +7,37 @@ This is a game where we have to help MacGyver to escape
 Mac Gyver must find 3 items to fight the guardian of the labyrinth
 """
 
+import pygame
+from pygame.locals import *
+
 import game
+import constants
 
 def main():
     """
     Main function of the programm
     """
+    #initialization of pygame module
+    pygame.init()
+    #creation of window for display
+    size_side = constants.number_cases_side*constants.size_case
+    window = pygame.display.set_mode((size_side, size_side))
+    #Title of window
+    pygame.display.set_caption(constants.title)
     board = game.Board()
     mac = game.Characters()
-    board.display(mac)
-    loop = 1
-    while loop:
-        direction = input("choose the direction: ")
-        mac.move(direction, board)
-        board.display(mac)
-        if board.STRUCTURE[mac.line][mac.column] == "a":
-            print("\nYOU WIN !")
-            break
+    #Display the labyrinth in the window
+    board.display(window)
+    play = 1
+    while play:
+        # direction = input("choose the direction: ")
+        # mac.move(direction, board)
+        # board.display(mac)
+        # if board.STRUCTURE[mac.line][mac.column] == "a":
+        #     print("\nYOU WIN !")
+        #     break
+        pygame.display.flip()
+        continue
 
 if __name__ == "__main__":
     main()
