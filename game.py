04a7325884bf5302bfada6230c96eadc673ed321
Florian <florian.lescuyer@gmail.com>
2017-09-27 18:15:59 +0200

    creation of class Position

--- game.py
+++ game.py
@@ -1,10 +1,11 @@
 """
 This file contains the classes of the programm
 There are 6 classes:
-     - Characters
+    - Board
+    - Position
+    - Characters
         - MacGyver
         - Murdoc
-    - Board
     - Items
     - Rules
 """
@@ -33,12 +34,7 @@ class Board:
         # Read the file "structure" and save the structure of the labyrinth
         # as a list in STRUCTURE[]
         with open("structure", "r") as labyrinth:
-            for line in labyrinth:
-                structure_lines = []
-                for letter in line:
-                    if letter != "\n":
-                        structure_lines.append(letter)
-                self.STRUCTURE.append(structure_lines)
+            self.STRUCTURE = [[letter for letter in line if letter != "\n"]for line in labyrinth]
 
     def display(self, window, character):
         """
@@ -51,8 +47,8 @@ class Board:
         for i, line in enumerate(self.STRUCTURE):
             for j, column in enumerate(line):
                 # we calculate the coordinates in pixels for pygame
-                coordinate_x = j * constants.size_sprite
-                coordinate_y = i * constants.size_sprite
+                coordinate_x = j * constants.sprite_size
+                coordinate_y = i * constants.sprite_size
                 if self.STRUCTURE[i][j] == "#":
                     window.blit(wall, (coordinate_x, coordinate_y))
                 elif self.STRUCTURE[i][j] == "h" or self.STRUCTURE[i][j] == "o":
@@ -69,6 +65,19 @@ class Board:
             window.blit(items.render(text_items, True, (255, 255, 255)), (120, 610))
 
 
+class Position:
+    """
+    This class manage the position of character and items
+    """
+    def __init__(self, line, column):
+        # Position in list STRUCTURE
+        self.column = column
+        self.line = line
+        # Position in pixels on the window
+        self.pixels_x = self.column * constants.sprite_size
+        self.pixels_y = self.line * constants.sprite_size
+
+
 class Characters:
     """
     This class is use for:
@@ -76,14 +85,10 @@ class Characters:
         .an avatar
         .a position
     """
-    def __init__(self):
+    def __init__(self, position):
         self.avatar = 'picture of character'
         # Position in list STRUCTURE
-        self.column = 'the letter of a line of list STRUCTURE'
-        self.line = 'the line of list STRUCTURE'
-        # Position in pixels on the window
-        self.pixels_x = 'x coordinate = self.colum * size_sprite'
-        self.pixels_y = 'y coordinate = self.line * size_sprite'
+        self.position = position
 
 
 class Macgyver(Characters):
@@ -94,14 +99,11 @@ class Macgyver(Characters):
         .his start position
         .the number of items
     - move MacGyver
-    - catch items
+    - catch if items
     """
-    def __init__(self):
+    def __init__(self, position):
         self.avatar = pygame.image.load(constants.macgyver).convert_alpha()
-        self.column = 0
-        self.line = 14
-        self.pixels_x = self.column * constants.size_sprite
-        self.pixels_y = self.line * constants.size_sprite
+        self.position = position
         self.number_items = 0
 
     def move(self, direction, board):
@@ -109,63 +111,54 @@ class Macgyver(Characters):
         This method allow the movement of the character in the labyrinth
         """
         if direction == "up":
-            if self.line > 0:
-                if board.STRUCTURE[self.line - 1][self.column] != "#":
-                    self.line -= 1
-                    self.pixels_y = self.line * constants.size_sprite
+            if self.position.line > 0:
+                if board.STRUCTURE[self.position.line - 1][self.position.column] != "#":
+                    self.position.line -= 1
+                    self.position.pixels_y = self.position.line * constants.sprite_size
                     # Movement with stairs
-                    if board.STRUCTURE[self.line][self.column] == "o":
-                        self.column = 4 # Position of stairs in STRUCTURE
-                        self.line = 3 # Position of stairs in STRUCTURE
-                        self.pixels_x = self.column * constants.size_sprite
-                        self.pixels_y = self.line * constants.size_sprite
+                    if board.STRUCTURE[self.position.line][self.position.column] == "o":
+                        self.position = Position(3, 4)
         if direction == "down":
-            if self.line < 14:
-                if board.STRUCTURE[self.line + 1][self.column] != "#":
-                    self.line += 1
-                    self.pixels_y = self.line * constants.size_sprite
+            if self.position.line < 14:
+                if board.STRUCTURE[self.position.line + 1][self.position.column] != "#":
+                    self.position.line += 1
+                    self.position.pixels_y = self.position.line * constants.sprite_size
                     # Movement with stairs
-                    if board.STRUCTURE[self.line][self.column] == "h":
-                        self.column = 14 # Position of stairs in STRUCTURE
-                        self.line = 4 # Postion of stairs in STRUCTURE
-                        self.pixels_x = self.column * constants.size_sprite
-                        self.pixels_y = self.line * constants.size_sprite
+                    if  board.STRUCTURE[self.position.line][self.position.column] == "h":
+                        self.position = Position(4, 14)
         if direction == "left":
-            if self.column > 0:
-                if board.STRUCTURE[self.line][self.column - 1] != "#":
-                    self.column -= 1
-                    self.pixels_x = self.column * constants.size_sprite
+            if self.position.column > 0:
+                if board.STRUCTURE[self.position.line][self.position.column - 1] != "#":
+                    self.position.column -= 1
+                    self.position.pixels_x = self.position.column * constants.sprite_size
         if direction == "right":
-            if self.column < 14:
-                if board.STRUCTURE[self.line][self.column + 1] != "#":
-                    self.column += 1
-                    self.pixels_x = self.column * constants.size_sprite
+            if self.position.column < 14:
+                if board.STRUCTURE[self.position.line][self.position.column + 1] != "#":
+                    self.position.column += 1
+                    self.position.pixels_x = self.position.column * constants.sprite_size
                     # Movement with stairs
-                    if board.STRUCTURE[self.line][self.column] == "o":
-                        self.column = 4 # Position of stairs in STRUCTURE
-                        self.line = 3 # Position of stairs in STRUCTURE
-                        self.pixels_x = self.column * constants.size_sprite
-                        self.pixels_y = self.line * constants.size_sprite
+                    if board.STRUCTURE[self.position.line][self.position.column] == "o":
+                        self.position = Position(3, 4)
 
-    def catch(self, needle, plastic, poison):
-        if needle.column == self.column and needle.line == self.line:
+    def catch_if_item(self, needle, plastic, poison):
+        if needle.column == self.position.column and needle.line == self.position.line:
             self.number_items += 1
             needle.column = 2
             needle.line = 15
-            needle.pixels_x = needle.column * constants.size_sprite
-            needle.pixels_y = needle.line * constants.size_sprite
-        if plastic.column == self.column and plastic.line == self.line:
+            needle.pixels_x = needle.column * constants.sprite_size
+            needle.pixels_y = needle.line * constants.sprite_size
+        if plastic.column == self.position.column and plastic.line == self.position.line:
             self.number_items += 1
             plastic.column = 2
             plastic.line = 15
-            plastic.pixels_x = plastic.column * constants.size_sprite
-            plastic.pixels_y = plastic.line * constants.size_sprite
-        if poison.column == self.column and poison.line == self.line:
+            plastic.pixels_x = plastic.column * constants.sprite_size
+            plastic.pixels_y = plastic.line * constants.sprite_size
+        if poison.column == self.position.column and poison.line == self.position.line:
             self.number_items += 1
             poison.column = 2
             poison.line = 15
-            poison.pixels_x = poison.column * constants.size_sprite
-            poison.pixels_y = poison.line * constants.size_sprite
+            poison.pixels_x = poison.column * constants.sprite_size
+            poison.pixels_y = poison.line * constants.sprite_size
 
 
 class Murdoc(Characters):
@@ -175,12 +168,9 @@ class Murdoc(Characters):
         .an avatar
         .his position
     """
-    def __init__(self):
+    def __init__(self, position):
         self.avatar = pygame.image.load(constants.murdoc).convert_alpha()
-        self.column = 14
-        self.line = 0
-        self.pixels_x = self.column * constants.size_sprite
-        self.pixels_y = self.line * constants.size_sprite
+        self.position = position
 
 
 class Items:
@@ -191,16 +181,16 @@ class Items:
         .a random position on the game board
     """
     def __init__(self, board):
-        self.avatar = pygame.image.load(constants.item).convert_alpha()
-        self.column = random.randint(0, constants.number_cases_side - 1)
-        self.line = random.randint(0, constants.number_cases_side - 1)
+        self.picture = pygame.image.load(constants.item).convert_alpha()
+        self.column = random.randint(0, len(board.STRUCTURE) - 1)
+        self.line = random.randint(0, len(board.STRUCTURE) - 1)
         # We check the random position is not a wall, a stair, start position
         # or arrival position
         while board.STRUCTURE[self.line][self.column] != ".":
-            self.column = random.randint(0, constants.number_cases_side - 1)
-            self.line = random.randint(0, constants.number_cases_side - 1)
-        self.pixels_x = self.column * constants.size_sprite
-        self.pixels_y = self.line * constants.size_sprite
+            self.column = random.randint(0, len(board.STRUCTURE) - 1)
+            self.line = random.randint(0, len(board.STRUCTURE) - 1)
+        self.pixels_x = self.column * constants.sprite_size
+        self.pixels_y = self.line * constants.sprite_size
 
 
 class Rules:
@@ -211,10 +201,8 @@ class Rules:
     def win(self, character, board):
         pygame.init()
         window_end = pygame.display.set_mode((550, 380))
-        if board.STRUCTURE[character.line][character.column] == "a":
-            if character.number_items == 3:
-                window_end.blit(pygame.image.load(constants.win).convert(), (0, 0))
-                pygame.display.flip()
-            else:
-                window_end.blit(pygame.image.load(constants.lose).convert(), (0, 0))
-                pygame.display.flip()
+        if character.number_items == 3:
+            window_end.blit(pygame.image.load(constants.win).convert(), (0, 0))
+        else:
+            window_end.blit(pygame.image.load(constants.lose).convert(), (0, 0))
+        pygame.display.flip()
