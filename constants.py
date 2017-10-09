4e1b30daa6be067a96d29f52ee6bc706ccd73509
Florian <florian.lescuyer@gmail.com>
2017-10-09 10:50:41 +0200

    add comments

--- constants.py
+++ constants.py
@@ -2,17 +2,18 @@
 This file contains constants of game and paths
 of pictures
 """
-
+# Size of game window
 width_number_sprite = 15
-height_number_sprite = 16 # The line of the inventory
+height_number_sprite = 16 # 15 + The line of the inventory
 sprite_size = 40
 # Symbols of structure of labyrinth
 wall_symbol = "#"
 floor_symbol = "."
 stairs_symbol = "l"
 item_symbol = "i"
-
+# Title of the game
 title = "MacGyver - The Game"
+# Pictures for the game
 floor = "pictures/floor.png"
 walls = "pictures/walls.png"
 item = "pictures/item.png"
@@ -24,4 +25,5 @@ tube = "pictures/tube.png"
 poison = "pictures/poison.png"
 win = "pictures/win.jpg"
 lose = "pictures/lose.jpg"
+# Music for the game
 music = "music/macgyver_theme.wav"
